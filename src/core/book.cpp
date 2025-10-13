1d9dbb189a87fc6b0309c0393ebe2aaffd8d0939
Shahzaib Ahmed <shahzaib@ualberta.ca>
2025-10-12 18:34:27 -0600

    created book class header file and initialized implementation file

---
+++ src/core/book.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file book.cpp
+ * @brief Implementation of the Book class for the Personal Reading Management System (PRMS)
+ * 
+ * This file contains the implementation of the methods declared
+ * in the Book class header file.
+ * @author Shahzaib Ahmed
+ * @date October 12, 2025
+ */
+
+#include "book.h"
+#include <stdexcept>
+#include <algorithm>
